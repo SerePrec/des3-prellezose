707946123ba857af45249c9773d1ea33795f1d26
Sergio E. Prellezo <sereprec@gmail.com>
2021-11-17 11:00:30 -0300

    Desafío 3

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ productos.init();
 const PORT = process.env.PORT || 8080;
 
 app
-  .get("/productos", async (req, res) => res.send(await productos.getAll()))
+  .get("/productos", async (req, res) => {
+    const lista = await productos.getAll();
+    lista !== null
+      ? res.send(lista)
+      : res.send({
+          message: "No se pudo recuperar la infomación"
+        });
+  })
   .get("/productoRandom", async (req, res) => {
     const maxId = productos.nextId - 1;
     const randomId = Math.floor(Math.random() * maxId + 1);
@@ -22,7 +29,7 @@ app
   });
 
 const server = app.listen(PORT, () =>
-  console.log(`Servidor http escuchando en puerto ${server.address().port}`)
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
 );
 server.on("error", error =>
   console.log(`Ocurrió un error en el servidor:\n ${error}`)
